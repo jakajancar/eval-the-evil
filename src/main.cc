1453bde5c7008255161f7b3fe25002ec6047533b
Jaka Jančar <jaka@celtra.com>
2019-07-17 09:04:21 -0400

    timeout benchmarks

--- src/main.cc
+++ src/main.cc
@@ -4,86 +4,158 @@
 #include <boost/asio.hpp>
 #include "error-handling.h"
 #include "evaluation.h"
+#include "timer.h"
+#include <cmath>
 
 namespace po = boost::program_options;
 using boost::asio::ip::tcp;
+
+uint64_t clock_gettime_nanos(clockid_t clockid)
+{
+  struct timespec time;
+  if (clock_gettime(clockid, &time) != 0)
+    throw_with_trace(std::system_error(errno, std::generic_category(), "Cannot get time"));
+  return time.tv_sec * 1e9 + time.tv_nsec;
+}
+
+static inline timespec duration_to_timespec(std::chrono::nanoseconds duration)
+{
+    using namespace std::chrono;
+    seconds secs = duration_cast<seconds>(duration);
+    duration -= secs;
+    return timespec{secs.count(), duration.count()};
+}
+
+double bench_mutex_thread_id(uint64_t timeout, int num_iterations, clockid_t clockid)
+{
+  uint64_t accumulator = 0;
+
+  for (int i = 0; i < num_iterations; i++) {
+    std::mutex stop_mutex;
+    bool stop = false;
+
+    auto timer_cb = [&]{
+      std::lock_guard<std::mutex> lock(stop_mutex);
+      stop = true;
+    };
+    eval::Timer<decltype(timer_cb)> timer(std::chrono::nanoseconds(timeout), clockid, timer_cb);
+    uint64_t start = clock_gettime_nanos(clockid);
+    while (true) {
+      std::lock_guard<std::mutex> lock(stop_mutex);
+      if (stop)
+        break;
+    }
+    uint64_t end = clock_gettime_nanos(clockid);
+    uint64_t duration = end - start;
+    int64_t overshoot = duration - timeout;
+    accumulator += std::abs(overshoot);
+    // printf("overshoot = %f ms\n", (double)overshoot / 1e6);
+  }
+  return (double)accumulator/num_iterations;
+}
+
+double bench_nomutex_thread_id(uint64_t timeout, int num_iterations, clockid_t clockid)
+{
+  uint64_t accumulator = 0;
+
+  for (int i = 0; i < num_iterations; i++) {
+    bool stop = false; // unsafe
+
+    auto timer_cb = [&]{
+      stop = true;
+    };
+    eval::Timer<decltype(timer_cb)> timer(std::chrono::nanoseconds(timeout), clockid, timer_cb);
+    uint64_t start = clock_gettime_nanos(clockid);
+    while (true) {
+      if (stop)
+        break;
+    }
+    uint64_t end = clock_gettime_nanos(clockid);
+    uint64_t duration = end - start;
+    int64_t overshoot = duration - timeout;
+    accumulator += std::abs(overshoot);
+    // printf("overshoot = %f ms\n", (double)overshoot / 1e6);
+  }
+  return (double)accumulator/num_iterations;
+}
+
+bool stop = false; // unsafe
+void bench_signal_handler(int signo)
+{
+  stop = true;
+}
+
+double bench_signal(uint64_t timeout, int num_iterations, clockid_t clockid)
+{
+  uint64_t accumulator = 0;
+
+  struct sigaction action;
+  memset(&action, 0, sizeof(action));
+  action.sa_handler = &bench_signal_handler;
+  sigaction(SIGUSR1, &action, NULL);
+
+  for (int i = 0; i < num_iterations; i++) {
+    stop = false;
+
+    // Timer
+    struct sigevent event;
+    memset(&event, 0, sizeof(event));
+    event.sigev_notify = SIGEV_SIGNAL;
+    event.sigev_signo = SIGUSR1;
+
+    timer_t timer;
+    if (timer_create(clockid, &event, &timer) != 0) {
+      std::cout << "timer_create failed\n";
+      abort();
+    }
+
+    struct itimerspec timing;
+    memset(&timing, 0, sizeof(timing));
+    timing.it_value = duration_to_timespec(std::chrono::nanoseconds(timeout));
+    if (timer_settime(timer, 0, &timing, NULL) != 0) {
+      std::cout << "timer_settime failed\n";
+      abort();
+    }
+
+    uint64_t start = clock_gettime_nanos(clockid);
+    while (true) {
+      if (stop)
+        break;
+    }
+
+    uint64_t end = clock_gettime_nanos(clockid);
+    if (timer_delete(timer) != 0) {
+      std::cout << "timer_delete failed\n";
+      abort();
+    }
+    uint64_t duration = end - start;
+    int64_t overshoot = duration - timeout;
+    accumulator += std::abs(overshoot);
+    // printf("overshoot = %f ms\n", (double)overshoot / 1e6);
+  }
+  return (double)accumulator/num_iterations;
+}
+
+typedef double(*bench_method_ptr)(uint64_t, int, clockid_t);
 
 int main(int argc, char *argv[])
 {
   GlobalErrorHandler eh;
 
-  // Process arguments
-  int port;
-  int num_threads;
-  po::options_description desc("Allowed options");
-  desc.add_options()
-      ("help", "produce help message")
-      ("port", po::value<int>(&port)->default_value(1101), "port to listen on")
-      ("threads", po::value<int>(&num_threads)->default_value(std::thread::hardware_concurrency()), "number of threads (defaults to hardware concurrency)")
-  ;
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
-  if (vm.count("help")) {
-      std::cout << desc << "\n";
-      return 1;
+  int num_clocks = 4;
+  clockid_t clock_ids[] = {CLOCK_REALTIME, CLOCK_MONOTONIC, CLOCK_PROCESS_CPUTIME_ID, CLOCK_THREAD_CPUTIME_ID};
+  const char *clock_names[] = {"CLOCK_REALTIME", "CLOCK_MONOTONIC", "CLOCK_PROCESS_CPUTIME_ID", "CLOCK_THREAD_CPUTIME_ID"};
+
+  int num_methods = 3;
+  bench_method_ptr method_ptrs[] = {&bench_mutex_thread_id, &bench_nomutex_thread_id, &bench_signal};
+  const char *method_names[] = {"bench_mutex_thread_id", "bench_nomutex_thread_id", "bench_signal"};
+
+  for (int c = 0; c < num_clocks; c++) {
+    printf("%s:\n", clock_names[c]);
+
+    for (int m = 0; m < num_methods; m++) {
+      double avg_overshoot = (*(method_ptrs[m]))(10 * 1e6, 100, clock_ids[c]);
+      printf("  - %s = %f ms\n", method_names[m], (double)avg_overshoot / 1e6);
+    }
   }
-
-  // Prepare global evaluation context
-  eval::GlobalContext global_eval_context;
-
-  // Start threads
-  std::thread threads[num_threads];
-  for (int i = 0; i < num_threads; i++) {
-    threads[i] = std::thread([port]{
-      // Listen for TCP connections.
-      boost::asio::io_service io_service;
-      tcp::acceptor acceptor(io_service);
-      acceptor.open(tcp::v4());
-      acceptor.set_option(boost::asio::socket_base::reuse_address(true));
-      acceptor.set_option(boost::asio::detail::socket_option::boolean<SOL_SOCKET, SO_REUSEPORT>(true));
-      acceptor.bind(tcp::endpoint(tcp::v4(), port));
-      acceptor.listen();
-
-      // Prepare thread-level evaluation context
-      eval::ThreadContext thread_eval_context;
-      while (true)
-      {
-        // Prepare request-specific evaluation context
-        eval::RequestContext request_eval_context(&thread_eval_context);
-
-        // Accept a connection
-        tcp::socket sock(io_service);
-        acceptor.accept(sock);
-
-        // Read the request
-        boost::system::error_code error;
-        std::string *request_blob = new std::string();
-        std::size_t read_bytes = boost::asio::read(sock, boost::asio::dynamic_buffer(*request_blob), boost::asio::transfer_all(), error);
-        if (error != boost::asio::error::eof)
-          throw_with_trace(boost::system::system_error(error));
-        assert(request_blob->length() == read_bytes);
-
-        // Evaluate
-        char *response_blob;
-        size_t response_blob_length;
-        request_eval_context.handle_request(request_blob->c_str(), response_blob, response_blob_length);
-        delete request_blob; // no longer needed now
-
-        // Send response
-        std::size_t written_bytes = boost::asio::write(sock, boost::asio::buffer(response_blob, response_blob_length));
-        assert(written_bytes == response_blob_length);
-        sock.close();
-
-      } // run loop
-      acceptor.close();
-    }); // thread
-  }
-  std::cout << "eval-the-evil listening on port " << port << ".\n";
-
-  // Wait for threads to exit
-  for (auto &thread : threads) {
-    thread.join();
-  }
-  return 0;
 }
